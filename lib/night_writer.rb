b7617cb5398a3f01e8983ba2460a0bfc224d7736
Sam Walker <sg.walker327@gmail.com>
2023-01-15 17:41:31 -0700

    I1: initial setup and confirmation reconfig

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,16 +1,7 @@
-message = File.open(ARGV[0], "r")
-
-incoming_text = message.read
-
-message.close
-
-braille_text = incoming_text
-
-braille_file = File.open(ARGV[1], "w")
-
-braille_file.write(braille_text)
-
-braille_file.close
-require 'pry'; binding.pry
+input = File.open(ARGV[0], "r")
+output = File.open(ARGV[1], "w")
+incoming_text = input.read
+output_text = incoming_text
+output.write(output_text)
 
 puts "Created '#{ARGV[1]}' containing #{incoming_text.length} characters"
