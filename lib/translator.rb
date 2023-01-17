ba27baf40c478e53f94ebd15177e0c9336cbc415
Sam Walker <sg.walker327@gmail.com>
2023-01-16 19:40:13 -0700

    I2: fix array_format formatting

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,39 +21,8 @@ class Translator
     new_strings = eng_braille_array.each_slice(40).map do |array|
       array.transpose.filter_map do |letters|
         letters.join
-      end.join("\n").to_s
-    end
+      end.join("\n")
+    end.join("\n")
+    # require 'pry'; binding.pry
   end
-
-  # def eng_braille_translate
-  #   puts array_format
-  # end
-
-  # def braille_split
-  #   line1 = []
-  #   line2 = []
-  #   line3 = []
-  #   eng_braille_array.flatten.each_with_index do |character, index|
-  #     if (index + 1) % 3 == 0
-  #       line3.push(character)
-  #     elsif (index + 1) % 3 == 1
-  #       line1.push(character)
-  #     else
-  #       line2.push(character)
-  #     end
-  #   end
-  #   [line1, line2, line3]
-  #   # require 'pry'; binding.pry
-  # end
-  
-  # def eng_braille_translate
-  #   newline1 = braille_split[0].to_s.gsub(/[^0.]/, "")
-  #   newline2 = braille_split[1].to_s.gsub(/[^0.]/, "")
-  #   newline3 = braille_split[2].to_s.gsub(/[^0.]/, "")
-  #   require 'pry'; binding.pry
-    
-  #   new_lines = "#{newline1}\n" "#{newline2}\n" + "#{newline3}"
-  # end
 end
-
-# .scan(/.{1,40}/)
