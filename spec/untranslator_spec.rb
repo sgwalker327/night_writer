ac05d8b74d58bcbb62c5a50ec526ae11081f8d6e
Sam Walker <sg.walker327@gmail.com>
2023-01-17 16:27:10 -0700

    I3: updated test for multi letter translation and new method, updated braille_eng array to braille_array test

--- spec/untranslator_spec.rb
+++ spec/untranslator_spec.rb
@@ -9,10 +9,21 @@ RSpec.describe Untranslator do
     end
   end
 
-  describe '#braille_eng_array' do
-    it 'can take braille string input and find corresponding english character' do
+  describe '#braille_array' do
+    it 'can take braille string input and convert them to a usable organized nested array' do
       untranslator = Untranslator.new("0.\n..\n..")
-      expect(untranslator.braille_eng_array).to eq("a")
+      expect(untranslator.braille_array).to eq([["0.", "..", ".."]])
+      untranslator = Untranslator.new("0.0.\n..0.\n....")
+      expect(untranslator.braille_array).to eq([["0.", "..", ".."], ["0.", "0.", ".."]])
+      untranslator = Untranslator.new("0.0.00\n..0...\n......")
+      expect(untranslator.braille_array).to eq([["0.", "..", ".."], ["0.", "0.", ".."], ["00", "..", ".."]])
+    end
+  end
+
+  describe '#braille_eng_translate' do
+    it 'can take the nested braille array and transform it into a string of english characters' do
+      untranslator = Untranslator.new("0.0.00\n..0...\n......")
+      expect(untranslator.braille_eng_translation).to eq("abc")
     end
   end
 end
