f0c22bf8978543acda307b4da08c97f13fb7502f
Sam Walker <sg.walker327@gmail.com>
2023-01-16 20:47:36 -0700

    Beef up initial untranslator_spec test

--- spec/untranslator_spec.rb
+++ spec/untranslator_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 RSpec.describe Untranslator do
   describe '#initialize' do
     it 'exists and has attributes' do
-      untranslator = Untranslator.new("")
+      untranslator = Untranslator.new("0.\n..\n..")
+      expect(untranslator).to be_a(Untranslator)
+      expect(untranslator.input).to eq("0.\n..\n..")
     end
   end
+
+  
 end
