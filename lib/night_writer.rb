c8d1c06064f948d12666ba067e3d0716acd31f42
Sam Walker <sg.walker327@gmail.com>
2023-01-16 19:52:52 -0700

    I2: update night_writer with new method

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -5,8 +5,8 @@ incoming_text = input.read
 
 translator = Translator.new(incoming_text)
 
-output = File.open(ARGV[1], "w")
-output_text = translator.eng_braille_translate
+output = File.open(ARGV[1], "w") 
+output_text = translator.array_format
 output.write(output_text)
 
 puts "Created '#{ARGV[1]}' containing #{incoming_text.length} characters"
