a33a19395bd624f0b0368451be0ced8e7b01ae39
Sam Walker <sg.walker327@gmail.com>
2023-01-16 20:32:52 -0700

    Initial untranslator spec setup

---
+++ spec/untranslator_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+RSpec.describe Untranslator do
+  describe '#initialize' do
+    it 'exists and has attributes' do
+      untranslator = Untranslator.new("")
+    end
+  end
+end
