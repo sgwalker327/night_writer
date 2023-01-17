f919a2f9c019259e93920a810425d283991f309d
Sam Walker <sg.walker327@gmail.com>
2023-01-17 09:59:23 -0700

    edit night_reader w/ initial method

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,12 +1,12 @@
-# require './lib/untranslator'
+require './lib/untranslator'
 
 input = File.open(ARGV[0], "r")
 incoming_text = input.read
 
-untranslator = incoming_text
+untranslator = Untranslator.new(incoming_text)
 
 output = File.open(ARGV[1], "w") 
-output_text = untranslator
+output_text = untranslator.braille_eng_array
 output.write(output_text)
 
 puts "Created '#{ARGV[1]}' containing #{output_text.length} characters"
