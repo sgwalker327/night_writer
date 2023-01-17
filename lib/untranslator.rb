f9b09c82d5cc7d685298ba37539bf4aadc48a0ed
Sam Walker <sg.walker327@gmail.com>
2023-01-17 16:28:14 -0700

    I3: split up braille_eng_array method to braille_array and braille_eng_translation methods, translates up to 40 letters

--- lib/untranslator.rb
+++ lib/untranslator.rb
@@ -10,9 +10,29 @@ class Untranslator
     @input = input
   end
 
-  def braille_eng_array
-    input_splits = input.split("\n")
-    english = braille_dictionary[input_splits]
-    english
+  # def braille_eng_array #works for one letter
+  #   input_splits = input.split("\n")
+  #   braille_dictionary[input_splits]
+  # end
+  
+  # def braille_eng_array #works for 2 letters and only 2
+  #   input_splits = input.scan(/(..)(..)/).transpose.map do |char|
+  #       braille_dictionary[char]
+  #   end.join
+  # end
+
+  def braille_array
+    input_splits = input.split()
+    # require 'pry'; binding.pry
+    braille = input_splits.map do |line|
+      line.scan(/../)
+    end
+    braille.transpose
+  end
+
+  def braille_eng_translation
+    braille_array.map do |char|
+      braille_dictionary[char]
+    end.join
   end
 end
