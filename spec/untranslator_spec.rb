3850a20f06cbef2b110c720869f502b2e93415d6
Sam Walker <sg.walker327@gmail.com>
2023-01-17 17:20:27 -0700

    Update multi character translation test

--- spec/untranslator_spec.rb
+++ spec/untranslator_spec.rb
@@ -12,11 +12,11 @@ RSpec.describe Untranslator do
   describe '#braille_array' do
     it 'can take braille string input and convert them to a usable organized nested array' do
       untranslator = Untranslator.new("0.\n..\n..")
-      expect(untranslator.braille_array).to eq([["0.", "..", ".."]])
+      expect(untranslator.braille_array).to eq([[["0.", "..", ".."]]])
       untranslator = Untranslator.new("0.0.\n..0.\n....")
-      expect(untranslator.braille_array).to eq([["0.", "..", ".."], ["0.", "0.", ".."]])
+      expect(untranslator.braille_array).to eq([[["0.", "..", ".."], ["0.", "0.", ".."]]])
       untranslator = Untranslator.new("0.0.00\n..0...\n......")
-      expect(untranslator.braille_array).to eq([["0.", "..", ".."], ["0.", "0.", ".."], ["00", "..", ".."]])
+      expect(untranslator.braille_array).to eq([[["0.", "..", ".."], ["0.", "0.", ".."], ["00", "..", ".."]]])
     end
   end
 
@@ -24,6 +24,8 @@ RSpec.describe Untranslator do
     it 'can take the nested braille array and transform it into a string of english characters' do
       untranslator = Untranslator.new("0.0.00\n..0...\n......")
       expect(untranslator.braille_eng_translation).to eq("abc")
+      untranslator = Untranslator.new(".00.0...000..0000...0.0.0..000..000.00...00.0000.0..0.0.0.0....00.0...0.0.0.00..\n0000.0..00..0.......0.00.000.0..0..0....00....0.0....00..000..0000.0..0....0.0..\n0.......0.00....0.....0.0..00.....0.00....000.0.0...0.00..0...0.......0...0000..\n000.00\n.0.000\n..0...")
+      expect(untranslator.braille_eng_translation).to eq("the quick brown fox jumps over the lazy dog")
     end
   end
 end
