97bae4c90c5fd0971283b3cf707435e297d5269a
Sam Walker <sg.walker327@gmail.com>
2023-01-17 17:37:45 -0700

    updated braille_eng translation test to incorporate line limit

--- spec/untranslator_spec.rb
+++ spec/untranslator_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 RSpec.describe Untranslator do
+
   describe '#initialize' do
     it 'exists and has attributes' do
       untranslator = Untranslator.new("0.\n..\n..")
@@ -21,11 +22,12 @@ RSpec.describe Untranslator do
   end
 
   describe '#braille_eng_translate' do
-    it 'can take the nested braille array and transform it into a string of english characters' do
+    it 'can take the nested braille array and transform it into 40 chr strings of english characters' do
       untranslator = Untranslator.new("0.0.00\n..0...\n......")
       expect(untranslator.braille_eng_translation).to eq("abc")
       untranslator = Untranslator.new(".00.0...000..0000...0.0.0..000..000.00...00.0000.0..0.0.0.0....00.0...0.0.0.00..\n0000.0..00..0.......0.00.000.0..0..0....00....0.0....00..000..0000.0..0....0.0..\n0.......0.00....0.....0.0..00.....0.00....000.0.0...0.00..0...0.......0...0000..\n000.00\n.0.000\n..0...")
-      expect(untranslator.braille_eng_translation).to eq("the quick brown fox jumps over the lazy dog")
+      expect(untranslator.braille_eng_translation).to eq("the quick brown fox jumps over the lazy \ndog")
+      
     end
   end
 end
